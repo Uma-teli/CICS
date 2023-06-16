bfdcd320f31470943524d4c0eb5dab4d638d67b2
Nagaraj <nagaraj.pk@tcs.com>
2023-06-16 12:41:58 +0000

    commit

--- samples/FraudDetection/scripts/gitCommit.sh
+++ samples/FraudDetection/scripts/gitCommit.sh
@@ -6,4 +6,5 @@ git add .
 
 git commit -m 'commit'
 
-git push origin master 'root:ibmuser@7'
+#git push origin master 'root:ibmuser@7'
+git push "http://root:'ibmuser@7'@169.38.108.133:9030/root/wmlzappbuild.git"
