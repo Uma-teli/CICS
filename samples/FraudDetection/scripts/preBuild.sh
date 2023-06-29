9db1082368e8606a406efb0dda5da5ea26160bcb
Nagaraj <nagaraj.pk@tcs.com>
2023-06-29 05:45:02 -0500

    fixed some bug

--- samples/FraudDetection/scripts/preBuild.sh
+++ samples/FraudDetection/scripts/preBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-MODEL_VERSION=$(python ${WORKSPACE}/scripts/json_extract.py model_version)
+MODEL_VERSION=$(python /u/wml/jenkinsPipeline/WMLzAppBuild/scripts/json_extract.py model_version)
 
 /u/wml/jenkinsPipeline/WMLzAppBuild/samples/FraudDetection/scripts/extract.sh $MODEL_VERSION /u/wml/jenkinsPipeline/WMLzAppBuild /var/jenkins/workspaces/WMLzCobolPipeline
 
