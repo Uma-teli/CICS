d8b6d98c4d788a98d644fd42852de3921a7b4654
Nagaraj <nagaraj.pk@tcs.com>
2023-08-14 06:57:37 +0000

    2023-08-14 06:57:37 - Automatic Commit - master
    Modified files:

--- samples/FraudDetection/cobol/FRAUDMOD.cbl
+++ samples/FraudDetection/cobol/FRAUDMOD.cbl
@@ -5,7 +5,7 @@
 
        01 I PIC 9(2) VALUE 1.
        01 MODEL_ID  PIC X(36) VALUE
-                    'd343d473-4e49-42a2-ac85-008542e7a2ae'.
+                    '2b1449b0-e2a4-4fbc-8bf7-b046d0a92b4c'.
        01 IN_CLASS  PIC X(16) VALUE 'FraudMLINwrapper'.
        01 OUT_CLASS PIC X(16) VALUE 'FraudMLOTwrapper'.
 
