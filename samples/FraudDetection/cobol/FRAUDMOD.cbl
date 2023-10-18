3cc20a718a20ccfb8ff0879d3846aab6cd377b06
OMVSKERN <OMVSKERN@S0W1.DAL-EBIS.IHOST.COM>
2023-10-18 18:28:46 +0000

    2023-10-18 18:28:45 - Automatic Commit - main
    Modified files:

--- samples/FraudDetection/cobol/FRAUDMOD.cbl
+++ samples/FraudDetection/cobol/FRAUDMOD.cbl
@@ -5,7 +5,7 @@
 
        01 I PIC 9(2) VALUE 1.
        01 MODEL_ID  PIC X(36) VALUE
-                    '16a5f819-cc84-4923-ac08-6fdab8f5f03e'.
+                    '23672839-c5fc-4447-bac9-f79a7117889f'.
        01 IN_CLASS  PIC X(16) VALUE 'FraudMLINwrapper'.
        01 OUT_CLASS PIC X(16) VALUE 'FraudMLOTwrapper'.
 
